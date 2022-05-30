228030226a5504fcd960cc72643c1c731c88b973
ImPrashantt <prashant33968@gmail.com>
2022-05-30 14:00:08 +0000

    Add `fs` modifications
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- fs/f2fs/node.c
+++ fs/f2fs/node.c
@@ -3,6 +3,7 @@
  * fs/f2fs/node.c
  *
  * Copyright (c) 2012 Samsung Electronics Co., Ltd.
+ * Copyright (C) 2021 XiaoMi, Inc.
  *             http://www.samsung.com/
  */
 #include <linux/fs.h>
@@ -1814,6 +1815,7 @@ static bool flush_dirty_inode(struct page *page)
 
 int f2fs_flush_inline_data(struct f2fs_sb_info *sbi)
 {
+
 	pgoff_t index = 0;
 	struct pagevec pvec;
 	int nr_pages;
@@ -1922,8 +1924,12 @@ continue_unlock:
 				goto continue_unlock;
 			}
 
-			/* flush inline_data, if it's async context. */
-			if (do_balance && is_inline_node(page)) {
+			/* flush inline_data/inode, if it's async context. */
+			if (!do_balance)
+				goto write_node;
+
+			/* flush inline_data */
+			if (is_inline_node(page)) {
 				clear_inline_node(page);
 				unlock_page(page);
 				flush_inline_data(sbi, ino_of_node(page));
@@ -1937,6 +1943,7 @@ continue_unlock:
 					goto lock_node;
 			}
 
+write_node:
 			f2fs_wait_on_page_writeback(page, NODE, true, true);
 
 			if (!clear_page_dirty_for_io(page))
